01362a2b5b0fbef0e3f855af1fefb91a6e69dc03
huytran1911 <tranhuy19112004@gmail.com>
2023-12-01 14:57:21 +0700

    new
    new

--- WebProject-main/trangsp.html/Chitietsp/js/js1.js
+++
@@ -1,8 +0,0 @@
-const Menu = document.querySelector('.box-menu');
-const MenuList = document.querySelector('.menu-list');
-const none = MenuList.querySelector('.none')
-const Drop = Menu.querySelector('i');
-console.log(none);
-Drop.onclick = function() {
-    none.classList.toggle('none');
-}
